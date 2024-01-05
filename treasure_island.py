b3e7b94c68349c30ac296eed793d15e21958e524
merihl <53103443+merihl@users.noreply.github.com>
2024-01-05 10:59:03 -0600

    Update treasure_island.py

--- treasure_island.py
+++ treasure_island.py
@@ -1,27 +1,26 @@
-# Treasure Island: treasure finder program
-
 directions = input("which direction do you want to go? left or right.\n")
-action = input("Do want to swim or go to the wall? choose swim or wall.\n")
-door_choice = input("Which door do you want to choose? yellow, red or blue.\n")
 
-if directions is left:
-  if action is wall:
-    if door_choice is yellow:
+if directions == "right":
+  print("Fall into a hole.\n", "Game Over!")
+  
+elif directions == "left":
+  action = input("Do want to swim or go to the wall? choose swim or wall.\n")
+  
+  if action == "swim":
+    print("Attacked by trout.\n", "Game Over")
+  elif action == "wall":
+    door_choice = input("Which door do you want to choose? yellow, red or blue.\n")
+    
+    if door_choice == "yellow":
       print("You Win!!!!!!!!!!!")
-    elif door_choice is red:
+    elif door_choice == "red":
       print("Burned by fire \n", "Game Over!")
-    elif door_choice is yellow:
+    elif door_choice == "blue":
       print("Eaten by beasts.\n", "Game Over")
     else:
       print("Game Ober!")
-
   else:
-    print("Attacked by trout.\n", "Game Over")
-
+    print("Game Over!!")
+  
 else:
-  print("Fall into a hole.\n", "Game Over!")
-
-
-      
-    
-
+  print("Game Over!!")
